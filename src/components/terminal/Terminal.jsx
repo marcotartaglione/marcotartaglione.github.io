4bd8dab63430cb15540b6315840400ffb0f24aa9
Marco Tartaglione <akenatotartaglione@gmail.com>
2024-03-06 22:33:47 +0100

    UI fix

--- src/components/terminal/Terminal.jsx
+++ src/components/terminal/Terminal.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import style from './Terminal.module.css';
 import {useTerminal} from "../../contexts/terminal/TerminalContext";
 
 export function Terminal({children}) {
     const terminalData = useTerminal();
 
+    const ref = useRef();
+
     return (
-        <div className={style.container}>
+        <div className={style.container} ref={ref}>
             {terminalData.lines.map((item, index) =>
                 <React.Fragment key={index} >
                     {item}
