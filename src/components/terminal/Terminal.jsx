70f3fcabce2ddf5a10eed4e0ef548dd7287bcff4
Marco Tartaglione <akenatotartaglione@gmail.com>
2024-07-30 19:53:20 +0200

    Fixed scroll issue

--- src/components/terminal/Terminal.jsx
+++ src/components/terminal/Terminal.jsx
@@ -1,21 +1,41 @@
-import React, {useRef} from "react";
+import React, { useEffect, useRef } from "react";
 import style from './Terminal.module.css';
-import {useTerminal} from "../../contexts/terminal/TerminalContext";
-import {Booting} from "../../boot/Booting";
+import { useTerminal } from "../../contexts/terminal/TerminalContext";
+import { Booting } from "../../boot/Booting";
 
-export function Terminal({children}) {
+export function Terminal({ children }) {
     const terminalData = useTerminal();
     const ref = useRef(null);
+    const clickPerformer = useRef(null);
+
+    useEffect(() => {
+        if (ref.current) {
+            requestAnimationFrame(() => {
+                ref.current.scrollIntoView({ behavior: "smooth" });
+            });
+        }
+    }, [terminalData.lines]);
+
+    useEffect(() => {
+        if (clickPerformer.current) {
+            clickPerformer.current.dispatchEvent(
+                new MouseEvent("mouseup", { bubbles: false })
+            );
+        }
+    }, [terminalData.lines]);
 
     return (
         <>
             <div className={style.containerMobile}>
                 <p>Hi there!</p>
-                <p style={{fontSize: "1rem", paddingBottom: "50px"}}>For the best experience, please visit this website on your PC</p>
+                <p style={{ fontSize: "1rem", paddingBottom: "50px" }}>
+                    For the best experience, please visit this website on your PC
+                </p>
                 <p>-M. Tartaglione</p>
             </div>
             <div className={style.container} ref={ref}>
-            <Booting/>
+                <Booting />
+                <div ref={clickPerformer} />
                 {terminalData.lines.map((item, index) => (
                     <React.Fragment key={index}>
                         {item}
